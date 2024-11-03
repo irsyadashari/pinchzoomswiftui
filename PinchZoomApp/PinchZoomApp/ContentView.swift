ba512bbf6e0d13264ca6ccb5df9b5c1cb7e31a4b
Irsyad Ashari <muh.ashari@gojek.com>
2024-11-03 14:03:19 +0700

    init commit

---
+++ PinchZoomApp/PinchZoomApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PinchZoomApp
+//
+//  Created by Muh Irsyad Ashari on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PinchZoomApp/PinchZoomApp/PinchZoomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchZoomAppApp.swift
+//  PinchZoomApp
+//
+//  Created by Muh Irsyad Ashari on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchZoomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
